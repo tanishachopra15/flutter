c9fea8dc2ca004c49afcaf3c1fbec718d045e0ac
Tanisha Chopra <tanishac768@gmail.com>
2024-05-14 11:54:51 +0530

    flutter widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,210 +1,109 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _width = 400.0;
+  var _height = 200.0;
+  bool flag = false;
+
+  @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Widgets"),
+          backgroundColor: Colors.blue[400],
+          leading: Icon(Icons.menu),
+          actions: [
+            IconButton(onPressed: () => {}, icon: Icon(Icons.search)),
+            IconButton(onPressed: () => {}, icon: Icon(Icons.more_vert)),
+          ],
         ),
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-
-  var favorites = <WordPair>[];
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
-    } else {
-      favorites.add(current);
-    }
-    notifyListeners();
-  }
-
-  void deleteFavorite(WordPair pair){
-    favorites.remove(pair);
-    notifyListeners();
-  }
-
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var selectedIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = GeneratorPage();
-        break;
-      case 1:
-        page = FavoritesPage();
-        break;
-      default:
-        throw UnimplementedError('no widget for $selectedIndex');
-    }
-    return Scaffold(
-      body: Row(
-        children: [
-          SafeArea(
-            child: NavigationRail(
-              extended: false,
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(Icons.home),
-                  label: Text('Home'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.favorite),
-                  label: Text('Favorites'),
+        body: Column(
+          children: [
+            ExpansionTile(
+              title: const Text('Expansion Tile'),
+              children: [
+                Container(
+                  padding: EdgeInsets.all(30),
+                  color: Colors.amber,
+                  alignment: Alignment.center,
+                  child: Text(
+                    'This is an Expansion Tile',
+                    style: TextStyle(fontSize: 20),
+                  ),
                 ),
               ],
-              selectedIndex: selectedIndex,
-              onDestinationSelected: (value) {
-                setState(() {
-                  selectedIndex = value;
-                });
-              },
             ),
-          ),
-          Expanded(
-            child: Container(
-              color: Theme.of(context).colorScheme.primaryContainer,
-              child: page,
+            SizedBox(
+              child: AnimatedContainer(
+                width: _width,
+                height: _height,
+                duration: Duration(seconds: 2),
+                child: Container(
+                  padding: EdgeInsets.all(30),
+                  color: Colors.green[400],
+                  alignment: Alignment.center,
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://cdn.pixabay.com/photo/2020/10/23/17/52/fox-5679446_960_720.png'),
+                    radius: 80,
+                    child: Text("Hello"),
+                  ),
+                ),
+              ),
             ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+            ElevatedButton(onPressed: () {
+              setState(() {
+                if(flag)
+                {
+                  _width = 400.0;
+                  _height = 200.0;
+                   flag = false;
+                }
+                else{
+                  _width = 200.0;
+                  _height = 200.0;
+                  flag = true; 
+                }
 
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
+              });
+            }, child: Text('Animation')),
+            Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                // padding: EdgeInsets.all(30),
+                color: Colors.blue[200],
+                child: Text("Expanded", style: TextStyle(fontSize: 20)),
               ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                alignment: Alignment.center,
+                // padding: EdgeInsets.all(30),
+                color: Colors.blueGrey,
+                child: Text(
+                  "Flexible",
+                   style: TextStyle(fontSize: 20)
+                ),
               ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-    var style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          pair.asLowerCase,
-          style: style,
-          semanticsLabel: pair.asPascalCase,
+            )
+          ],
         ),
       ),
-    );
-  }
-}
-
-class FavoritesPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('No favorites yet.'),
-      );
-    }
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: IconButton(icon: Icon(Icons.delete), onPressed: (){
-              appState.deleteFavorite(pair);
-            },),
-            title: Text(pair.asLowerCase),
-          ),
-      ],
     );
   }
 }
